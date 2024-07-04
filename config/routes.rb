0bbada3af37d0bb211f04bb13560893838eb07ae
safych <v7lad@ukr.net>
2024-07-04 22:28:39 +0300

    Added profile, users and update for user_links

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
-  resources :user_links, only: [:create, :destroy]
+  resources :user_links, only: [:create, :update, :destroy]
+  resource :profile, only: :show
+  resources :users, only: :update
 
   root "home#index"
 end
