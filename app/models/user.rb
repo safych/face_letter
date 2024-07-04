77917657327d11c429eb7399b7a60198f82c5e63
safych <v7lad@ukr.net>
2024-07-04 12:44:35 +0300

    Added validations for email, name and surname

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,19 @@
 class User < ApplicationRecord
-  has_many :users
+  has_many :user_link, dependent: :destroy
+  has_one_attached :avatar
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
+  validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@.+\.\S+\z/, message: :invalid }
   validates :password, presence: true, length: { minimum: 8, maximum: 128 }
   validates :password, format: { 
     with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/,
     message: I18n.t("password_validates_format")
   }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :surname, presence: true, length: { maximum: 50 }
 
   def self.from_google(u)
     create_with(uid: u[:uid], provider: 'google',
