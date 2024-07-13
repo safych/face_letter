6210d5d18fa1ddfe86269e86b49015cbc5e52d2f
safych <v7lad@ukr.net>
2024-07-13 13:22:02 +0300

    Updated views, before_action was changed on prepend_before_action

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  before_action :check_captcha, only: [:create]
+  prepend_before_action :check_captcha, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
