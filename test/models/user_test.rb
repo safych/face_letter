45239e6199f0ae414a93fb24cb33331b244ffe4a
safych <v7lad@ukr.net>
2024-07-14 16:11:59 +0300

    Created tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "af", surname: "fdg", password: "1Fdses2!", email: "test@gmail.com")
+  end
+
+  test "valid user" do
+    assert @user.valid?
+  end
+
+  test 'invalid without password' do
+    @user.password = nil
+    refute @user.valid?, 'user is valid without a password'
+    assert_not_nil @user.errors[:password], 'no validation error for password present'
+  end
+
+  test 'invalid with incorrect password format' do
+    @user.password = '123d13df'
+    refute @user.valid?, 'user is valid with incorrect password format'
+    assert_includes @user.errors[:password], I18n.t("models.user.password_validates_format"), 'Password format error message not found'
+  end
+
+  test 'invalid with incorrect name and surname length' do
+    @user.name = 'sdfgggggggggggggggggggggggggggggvtrvrtgrrrrrrrrrrrrrrrrrrrrrrrr'
+    @user.surname = 'sdfgggggggggggggggggggggggggggggvtrvrtgrrrrrrrrrrrrrrrrrrrrrr'
+    refute @user.valid?, 'user is valid with incorrect name and surnamme length'
+    assert_includes @user.errors[:name], "is too long (maximum is 50 characters)", "Name length error message not found"
+    assert_includes @user.errors[:surname], "is too long (maximum is 50 characters)", "Surname length error message not found"
+  end
+
+  test 'invalid email format' do
+    @user.email = "123@frrfer"
+    refute @user.valid?, 'user is valid with invalid email format'
+    assert_includes @user.errors[:email], "is invalid", "Email format error message not found"
+  end
 end
