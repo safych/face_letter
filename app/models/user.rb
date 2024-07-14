3c9187bad501f903bd7ba548d17dfb638cc94036
safych <v7lad@ukr.net>
2024-07-14 14:01:42 +0300

    Updated locale for password's format message

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 8, maximum: 128 }
   validates :password, format: { 
     with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/,
-    message: I18n.t("password_validates_format")
+    message: I18n.t("models.user.password_validates_format")
   }
   validates :name, presence: true, length: { maximum: 50 }
   validates :surname, presence: true, length: { maximum: 50 }
