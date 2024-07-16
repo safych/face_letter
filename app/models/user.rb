74d1a677c8eea98e28898d3300bd32ff1e909a24
safych <v7lad@ukr.net>
2024-07-16 14:15:38 +0300

    Removed password's validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,6 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@.+\.\S+\z/, message: :invalid }
-  validates :password, presence: true, length: { minimum: 8, maximum: 128 }
-  validates :password, format: { 
-    with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/,
-    message: I18n.t("models.user.password_validates_format")
-  }, if: :password_required?
   validates :name, presence: true, length: { maximum: 50 }, unless: :google_auth?
   validates :surname, presence: true, length: { maximum: 50 }, unless: :google_auth?
 
@@ -24,10 +19,6 @@ class User < ApplicationRecord
   end
 
   private
-
-  def password_required?
-    provider.blank? || !password.nil?
-  end
 
   def google_auth?
     provider == 'google'
