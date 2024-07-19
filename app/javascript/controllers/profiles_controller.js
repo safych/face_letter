1f2efaf9cf97e8e35ef0607959f0fa77c2090f41
safych <v7lad@ukr.net>
2024-07-19 13:04:41 +0300

    Added toggleDisplay and hideElements functions

--- app/javascript/controllers/profiles_controller.js
+++ app/javascript/controllers/profiles_controller.js
@@ -1,39 +1,64 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class ProfilesController extends Controller {
+  connect() {
+    this.userLinks = document.getElementById('all_user_links');
+    this.userAvatarUpdateForm = document.getElementById('user_avatar_update_form');
+    this.userLinkCreateForm = document.getElementById('user_link_create_form');
+    this.userNameSurnameUpdateForm = document.getElementById('user_name_surname_update_form');
+    this.userEmailUpdateForm = document.getElementById('user_email_update_form');
+    this.userPasswordUpdateForm = document.getElementById('user_password_update_form');
+    this.array = [
+      this.userLinks, this.userAvatarUpdateForm, this.userLinkCreateForm,
+      this.userNameSurnameUpdateForm, this.userEmailUpdateForm, this.userPasswordUpdateForm
+    ];
+  }
+
   showAllUserLinks() {
-    const userLinks = document.getElementById('all_user_links');
-    userLinks.style.display = (userLinks.style.display === 'none') ? 'block' : 'none';
+    this.toggleDisplay(this.userLinks);
+    this.hideElements(this.userLinks);
     return false;
   }
 
   showUserAvatarUpdateForm() {
-    const userAvatarUpdateForm = document.getElementById('user_avatar_update_form');
-    userAvatarUpdateForm.style.display = (userAvatarUpdateForm.style.display === 'none') ? 'block' : 'none';
+    this.toggleDisplay(this.userAvatarUpdateForm);
+    this.hideElements(this.userAvatarUpdateForm);
     return false;
   }
 
   showUserLinkCreateForm() {
-    const userLinkCreateForm = document.getElementById('user_link_create_form');
-    userLinkCreateForm.style.display = (userLinkCreateForm.style.display === 'none') ? 'block' : 'none';
+    this.toggleDisplay(this.userLinkCreateForm);
+    this.hideElements(this.userLinkCreateForm);
     return false;
   }
 
   showUserNameSurnameUpdateForm() {
-    const userNameSurnameUpdateForm = document.getElementById('user_name_surname_update_form');
-    userNameSurnameUpdateForm.style.display = (userNameSurnameUpdateForm.style.display === 'none') ? 'block' : 'none';
+    this.toggleDisplay(this.userNameSurnameUpdateForm);
+    this.hideElements(this.userNameSurnameUpdateForm);
     return false;
   }
 
   showUserEmailUpdateForm() {
-    const userEmailUpdateForm = document.getElementById('user_email_update_form');
-    userEmailUpdateForm.style.display = (userEmailUpdateForm.style.display === 'none') ? 'block' : 'none';
+    this.toggleDisplay(this.userEmailUpdateForm);
+    this.hideElements(this.userEmailUpdateForm);
     return false;
   }
 
   showUserPasswordUpdateForm() {
-    const userPasswordUpdateForm = document.getElementById('user_password_update_form');
-    userPasswordUpdateForm.style.display = (userPasswordUpdateForm.style.display === 'none') ? 'block' : 'none';
+    this.toggleDisplay(this.userPasswordUpdateForm);
+    this.hideElements(this.userPasswordUpdateForm);
     return false;
+  }
+
+  toggleDisplay(element) {
+    element.style.display = (element.style.display === 'none') ? 'block' : 'none';
+  }
+
+  hideElements(element) {
+    for(let i = 0; i < this.array.length; i++) {
+      if (this.array[i] != element) {
+        this.array[i].style.display = 'none';
+      }
+    }
   }
 }
