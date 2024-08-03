19af3b4b34c175c2846646e2205c90314b342c3d
safych <v7lad@ukr.net>
2024-08-03 14:48:50 +0300

    Added setup for capybara and devise

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,15 +2,24 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 require "devise/test/integration_helpers"
+require "capybara/rails"
+require "capybara/minitest"
+
+Capybara.default_driver = :selenium_chrome
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
-    # Add more helper methods to be used by all tests here...
+    include Capybara::DSL
+    include Capybara::Minitest::Assertions
+    include Devise::Test::IntegrationHelpers
+  
+    teardown do
+      Capybara.reset_sessions!
+      Capybara.use_default_driver
+    end
   end
 end
